e59188060139d9e2fd751e2ebec350c1b115bd83
eNeville <17412822+eNeville@users.noreply.github.com>
2023-01-16 23:55:03 -0400

    Add images with webpack

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './icon.png';
 
 function component() {
   const element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
 
   return element;
 }
