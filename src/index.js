ad4832ab89abc66aceb13be8bdc60b34289b0744
eNeville <17412822+eNeville@users.noreply.github.com>
2023-01-17 00:08:18 -0400

    Load data with webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './icon.png';
+import Data from './data.xml';
+import Notes from './data.csv';
 
 function component() {
   const element = document.createElement('div');
@@ -12,6 +14,9 @@ function component() {
   myIcon.src = Icon;
 
   element.appendChild(myIcon);
+
+  console.log(Data);
+  console.log(Notes);
 
   return element;
 }
